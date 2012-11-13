9ac0bb28698825aa1ed22fc7adb9b0add2ef9b5e
Phil Booth <pmbooth@gmail.com>
2012-11-13 18:21:10 +0000

    Add initial routing.

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 'use strict';
 
 var express = require('express'),
+    hogan = require('hogan-express'),
     http = require('http'),
     path = require('path'),
     cr = require('complexity-report'),
-    app = express(),
-    page = fs.readFileSync('page.html', 'utf8');
+    app = express();
 
 app.configure(function () {
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
+    app.set('view engine', 'html');
+    app.enable('view cache');
+    app.engine(hogan);
     app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
@@ -24,25 +27,48 @@ app.configure('development', function () {
 });
 
 app.get('/', function (request, response) {
-    reponse.send(page);
+    reponse.render('report');
+});
+
+app.post('/' function (request, response) {
+    var report = getReportResponse(request);
+    response.render('report', report.body);
 });
 
 app.post('/report.json', function (request, response) {
-    var report;
-    try {
-        report = cr.run(request.body.source, {
-            forin: request.body.forin || false,
-            trycatch: request.body.trycatch || false,
-            logicalor: !request.body.logicalor,
-            switchcase: !request.body.switchcase
-        });
-        response.jsonp(200, report);
-    } catch (error) {
-        console.dir(error);
-        response.jsonp(500, { error: error.message });
-    }
+    var report = getReportResponse(request);
+    response.jsonp(report.status, report.body);
+});
+
+app.get('/complexity', function (request, response) {
+    response.render('complexity');
+});
+
+app.get('/about', function (request, response) {
+    response.render('about');
 });
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
 });
+
+function getReportResponse (request) {
+    try {
+        return {
+            status: 200,
+            body: cr.run(request.body.source, {
+                forin: request.body.forin || false,
+                trycatch: request.body.trycatch || false,
+                logicalor: !request.body.logicalor,
+                switchcase: !request.body.switchcase
+            }
+        };
+    } catch (error) {
+        return {
+            status: 500,
+            body: {
+                error: error.message
+            }
+        };
+    }
+}
