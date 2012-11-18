ff7cd3721837c03c91d4ba18075e4c4481fc57b0
Phil Booth <pmbooth@gmail.com>
2012-11-18 13:46:33 +0000

    Rename report => results.

--- views/partials/index.js
+++ views/partials/index.js
@@ -8,9 +8,9 @@ module.exports = {
     main: 'partials/body/main',
     source: 'partials/body/main/source',
     error: 'partials/body/main/error',
-    report: 'partials/body/main/report',
-    metrics: 'partials/body/main/report/metrics',
-    functions: 'partials/body/main/report/functions',
+    results: 'partials/body/main/results',
+    metrics: 'partials/body/main/results/metrics',
+    functions: 'partials/body/main/results/functions',
     complexity: 'partials/body/complexity',
     about: 'partials/body/about'
 };
