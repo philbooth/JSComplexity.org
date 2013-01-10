c8da3b0bb1b229317614cf581dd64756caea2409
Phil Booth <pmbooth@gmail.com>
2013-01-10 00:05:27 +0000

    Add meta description.

--- settings.js
+++ settings.js
@@ -4,14 +4,17 @@ module.exports = {
     title: 'JSComplexity.org',
     home: {
         path: '/',
-        label: 'Analysis'
+        label: 'Analysis',
+        description: 'Software complexity analysis for JavaScript projects.'
     },
     complexity: {
         path: '/complexity',
-        label: 'About complexity'
+        label: 'About complexity',
+        description: 'A discussion of software complexity metrics and how they are calculated.'
     },
     about: {
         path: '/about',
-        label: 'About this site'
+        label: 'About this site',
+        description: 'Links to the open-source projects that were used to build this site.'
     }
 };
